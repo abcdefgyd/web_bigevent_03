e2d4cc605ce939cc4bf9f31b17475f63dd8a84fa
gyd <1119284274@qq.com>
2020-11-27 14:33:54 +0800

    登陆注册功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,52 @@
+// // 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
+// // 会先调用 ajaxPrefilter 这个函数
+// // 在这个函数中，可以拿到我们给Ajax提供的配置对象
+// $.ajaxPrefilter(function (options) {
+//   // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
+//   options.url = 'http://ajax.frontend.itheima.net' + options.url
+
+//   //对需要权限的接口设置配置信息
+//   if (options.url.indexOf('/my/') !== -1) {
+//     options.header = {
+//       Authorization: localStorage.getItem('token') || ''
+//     }
+//   }
+
+//   //拦截所有响应，判断身份认证信息
+//   options.complete = function (res) {
+//     var obj = res.responseJSON
+//     if (obj.status == 1 && obj.message == '身份认证失败！') {
+//       //清空本地token
+//       localStorage.removeItem('token')
+
+//       //页面跳转
+//       location.href = '/login.html'
+//     }
+//   }
+// })
+var baseURL='http://ajax.frontend.itheima.net'
 // 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function(options) {
   // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-  options.url = 'http://ajax.frontend.itheima.net' + options.url
+  options.url = baseURL + options.url
+
+  //对需要权限的接口设置配置信息,必须以my开头的
+  if(options.url.indexOf('/my/')!==-1){
+    options.headers={
+      Authorization: localStorage.getItem('token') || ''
+    }
+  }
+//拦截所有响应，判断身份认证信息
+  options.complete=function(res){
+    var obj=res.responseJSON
+    if(obj.status==1&&obj.message=='身份认证失败！'){
+    //1清空本地token
+    localStorage.removeItem('token')
+    //2页面跳转
+    location.href='/login.html'
+
+    }
+  }
 })
